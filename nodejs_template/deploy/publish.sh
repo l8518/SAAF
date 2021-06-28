221aa49e71647fbbbd2a237b4ed6cc27a21fad97
Leonard Herold <git@hdld.me>
2021-06-28 11:28:36 +0200

    Increase to a minute

--- nodejs_template/deploy/publish.sh
+++ nodejs_template/deploy/publish.sh
@@ -170,7 +170,7 @@ if [[ ! -z $4 && $4 -eq 1 ]]; then
 	az functionapp create --resource-group $function --consumption-plan-location eastus --name $function --runtime $azureRuntime --os-type Linux --output json --storage-account $function --app-insights $function
 
 	# Wait to prevent failing command below:
-	sleep 30
+	sleep 60
 
 	echo
 	echo Deploying function... This may fail if the function app is brand new. In that event, please run this script again.
